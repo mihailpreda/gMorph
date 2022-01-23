311ec18836015e5dcbb1666004cc7d203095b6d9
Preda Mihail Irinel <mihaipreda1997@gmail.com>
2022-01-23 17:49:06 +0200

    Added division

--- src/algebra/m231.rs
+++ src/algebra/m231.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::convert::TryFrom;
 use std::fmt;
-use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign, DivAssign};
 
 const MODULUS: u32 = 2147483647u32; // 2^31 -1
 const MODULUSI32: i32 = 2147483647i32;
@@ -203,6 +203,14 @@ impl Div for Mod231 {
 
     fn div(self, other: Self) -> Self::Output {
         Self(normalize(self.0 * other.invert().0))
+    }
+}
+
+impl DivAssign for Mod231 {
+    type Output = Self;
+
+    fn div_assign(&mut self, other: Self) {
+        *self = self.div(other)
     }
 }
 
